3fd9684c0f6417bb7979ee01e0233f9c8ef2fbee
Biliana <bilianavaleva@gmail.com>
2021-01-07 11:09:28 +0100

    Add comments

--- compare.js
+++ compare.js
@@ -4,11 +4,12 @@ const string04 = '4';
 const isEqualByValue = number04 == string04; // true
 console.log(!isEqualByValue); // false
 
-
 const isEqualByValueAndType = number04 === string04; // false
-const isNotEqualByValueAndType = number04 !== string04; // true
-console.log(!isEqualByValueAndType);
+const isNotEqualByValueAndType = number04 !== string04; // true, is not equal to
+console.log(!isEqualByValueAndType); // true
+
+// !false -> true
+// !true -> false
 
 console.log('isEqualByValueAndType: ' + isEqualByValueAndType, 'isEqualByValue: ' + isEqualByValue);
-
 console.log('isNotEqualByValueAndType: ', isNotEqualByValueAndType);
